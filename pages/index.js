4170fd7f1ab2455406e0579355b18875785e6fca
Rid Tiff <sergey@campanelli.ru>
2021-11-26 13:05:40 +0300

    6 начальных карточек из JS

--- pages/index.js
+++ pages/index.js
@@ -81,3 +81,33 @@ closeButtonAddCard.addEventListener('click', function () {
 
 //Создание 6 начальных карточек
 
+const initialCards = [
+    {
+      name: 'Oxxxymiron',
+      link: 'https://worldpodium.ru/sites/default/files/reper_anons.jpg'
+    },
+    {
+      name: 'Неваляшка',
+      link: 'https://i09.fotocdn.net/s103/8ec5ca804cd027ee/user_l/2181749682.jpg'
+    },
+    {
+      name: 'Горгород',
+      link: 'https://avatars.mds.yandex.net/get-zen_doc/1062011/pub_5b6d65f1b89e8d00a9d23868_5b6d6cb2b2cd8f00abdd9dc4/scale_1200'
+    },
+    {
+      name: 'Мох',
+      link: 'https://i.ytimg.com/vi/CfCit3iVZGg/maxresdefault.jpg'
+    },
+    {
+      name: 'Кто убил Марка',
+      link: 'https://s0.rbk.ru/v6_top_pics/resized/1200xH/media/img/1/62/756375888740621.jpg'
+    },
+    {
+      name: 'Вагабунд',
+      link: 'https://avatars.mds.yandex.net/i?id=f3424c2484bb49761d2533931050079c-5101235-images-thumbs&n=13'
+    }
+  ]; 
+
+for (let i = 0; i < initialCards.length; i++) {
+    elements.prepend(createCard(initialCards[i].name, initialCards[i].link));
+}
