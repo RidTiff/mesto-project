937519c72b5000739a586e460ac6894d9f6cf475
Alexandr Shevelev <ash@x5z.tech>
2022-04-05 21:14:49 +0300

    api

--- src/components/api.js
+++ src/components/api.js
@@ -181,13 +181,13 @@ export class Api {
     }
 
     patchAvatar(avatar) {
-        return fetch('https://nomoreparties.co/v1/plus-cohort-6/users/me/avatar', {
+        return fetch(`${this.host}/users/me/avatar`, {
             method: 'PATCH',
             body: JSON.stringify ({
                 avatar: avatar
             }),
             headers: {
-                authorization: 'a5873ca2-eb5b-4cfd-9dad-a8ba3d811b6c',
+                authorization: this.authorization,
                 'Content-Type': 'application/json; charset=UTF-8'
             }
         })
@@ -196,4 +196,88 @@ export class Api {
             renderAvatar(result.avatar);
         })
     }
+
+    getCards(user) {
+        return fetch(`${this.host}/cards`, {
+            headers: {
+                authorization: this.authorization
+            }
+        })
+        .then((res) => checkResponse (res))
+        .then((cards) => {
+            cards.forEach(element => {
+                const userId = user._id;
+                let deleteCard = false;
+                let checkLike = false;
+                if (element.owner._id == userId) {
+                    deleteCard = true;
+                }
+                for (let i = 0; i < element.likes.length; i++) {
+                    if (element.likes[i]._id == userId) {
+                        checkLike = true;
+                        break
+                    }
+                };
+    
+                showCard(element.name, element.link, deleteCard, element.likes.length, checkLike, element._id);
+            });
+        })
+    }
+
+    postCard(name, link) {
+        return fetch(`${this.host}/cards`, {
+            method: 'POST',
+            body: JSON.stringify ({
+                name: name,
+                link: link
+            }),
+            headers: {
+                authorization: this.authorization,
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        })
+        .then((res) => checkResponse (res))
+        .then((result) => {
+            showCard(result.name, result.link, true, 0, false, result._id);
+        })
+    }
+
+    deleteCard(evt, id) {
+        return fetch(`${this.host}/cards/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: this.authorization
+            }
+        })
+        .then((res) => checkResponse (res))
+        .then(() => {
+            evt.target.closest('.card').remove()
+        })
+    }
+
+    putLike(id, countElement) {
+        return fetch(`${this.host}/cards/likes/${id}`, {
+            method: 'PUT',
+            headers: {
+                authorization: this.authorization
+            }
+        })
+        .then((res) => checkResponse (res))
+        .then((result) => {
+            renderLikes(countElement, result.likes.length);
+        })
+    }
+
+    deleteLike(id, countElement) {
+        return fetch(`${this.host}/cards/likes/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: this.authorization
+            }
+        })
+        .then((res) => checkResponse (res))
+        .then((result) => {
+            renderLikes(countElement, result.likes.length);
+        })
+
 }
